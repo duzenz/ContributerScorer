2b73ede8e9cff6b8faabd10089ab4ad696515e48
duzenz <duzenz@gmail.com>
2019-05-02 11:36:59 +0300

    client api skeleton

---
+++ service-content-scorer/src/main/java/com/service/scorer/controller/ApiExposeController.java
@@ -0,0 +1,24 @@
+package com.service.scorer.controller;
+
+import com.service.scorer.models.entity.RepoRank;
+import com.service.scorer.repository.RepoRankRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/score/api")
+public class ApiExposeController {
+
+    @Autowired
+    RepoRankRepository repoRankRepository;
+
+    @GetMapping(path = "/{repoName}")
+    public List<RepoRank> getRepoRanking(@PathVariable String repoName) {
+        return repoRankRepository.findTop20ByRepoNameOrderByScoreDesc(repoName);
+    }
+}
--- service-content-scorer/src/main/java/com/service/scorer/repository/RepoRankRepository.java
+++ service-content-scorer/src/main/java/com/service/scorer/repository/RepoRankRepository.java
@@ -3,5 +3,10 @@ package com.service.scorer.repository;
 import com.service.scorer.models.entity.RepoRank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepoRankRepository extends JpaRepository<RepoRank, Integer> {
+
+    List<RepoRank> findTop20ByRepoNameOrderByScoreDesc(String repoName);
+
 }
